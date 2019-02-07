fdd68c7f057b6ae8a2277510808f51c2393d5bcc
pragathigajjala <46912681+pragathigajjala@users.noreply.github.com>
2019-02-06 22:10:11 -0800

    Add project files.

---
+++ Mod2_Lab4/Program.cs
@@ -0,0 +1,29 @@
+﻿//This program demonstrate "while" Loop and "do while" Loop
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while(i<6)
+            {
+                Console.WriteLine($"current value of i is {i}");
+                i++;
+             }
+
+            int n = 0;
+            do
+            {
+                Console.WriteLine(n);
+                n++;
+            } while (n < 5);
+        }
+    }
+}
